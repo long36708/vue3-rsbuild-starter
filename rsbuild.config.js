6d396909e337ad9b673e36df28bde58a65e86e04
longmo <3131521901@qq.com>
2026-01-11 13:45:48 +0800

    chore: update dependencies and add build info log

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -1,16 +1,40 @@
 // @ts-check
 import { defineConfig } from '@rsbuild/core'
+// import { pluginBabel } from '@rsbuild/plugin-babel'
+import { pluginBasicSsl } from '@rsbuild/plugin-basic-ssl'
+import { pluginCheckSyntax } from '@rsbuild/plugin-check-syntax'
+import { pluginSass } from '@rsbuild/plugin-sass'
+// import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
 import { pluginVue } from '@rsbuild/plugin-vue'
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  plugins: [pluginVue()],
-  tools: {
-    rspack: {
-      // plugins: [Oxlint()]
+  plugins: [
+    pluginVue(),
+    pluginBasicSsl(),
+    pluginSass(),
+    pluginCheckSyntax(),
+    // pluginBabel(),
+    // pluginNodePolyfill(),
+  ],
+  server: {
+    cors: true,
+    headers: {
+      // 启用跨域隔离所需的三个核心头信息
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      // 建议也加上资源策略头
+      'Cross-Origin-Resource-Policy': 'same-origin',
     },
   },
-  source: {
+  tools: {
+    rspack: {
+      plugins: [require('longmo-unplugin-info/rspack')({
+        /* options */
+      })],
+    },
+  },
+  resolve: {
     alias: {
       '@': './src',
     },
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '~build/console'
 import './index.css'
 
 createApp(App).mount('#root')
+
+// console log the build info
+// console.log(`Build ${sha} at ${now}`)
